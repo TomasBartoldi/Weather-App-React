883bfb214ecbc37199b31bc1da567d75b84a095c
Tomas Bartoldi <bartoldit@gmail.com>
2022-07-19 14:58:03 -0300

    Weather-App

---
+++ weather-app/src/Components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Card extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render(){
+        return (
+          <div>
+            <button>X</button>
+            <h2>{this.props.name}</h2>
+            <div>
+                <div>
+                    <h2>Min</h2>
+                    <h2>{this.props.min}°</h2>
+                </div>
+                <div>
+                    <h2>Min</h2>
+                    <h2>{this.props.max}°</h2>
+                </div>
+                <img src="#" />
+            </div>
+          </div>
+        )
+      }
+    };
+
+export default Card
---
+++ weather-app/src/Components/Nav/Nav.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import './Nav.css'
+
+class Nav extends React.Component {
+    constructor({onSearch}) {
+      super(onSearch);
+      this.state = {
+        city: ''
+      }
+    }
+    updateState = (e) => {
+      this.setState({
+        city: e.target.value
+      })
+    }
+
+    render() {
+        return (
+
+
+          <form 
+          className="container-header"
+          onSubmit={(e) => {
+          e.preventDefault();
+          this.onSearch(this.state.city); 
+          this.setState({city:''})
+          }}
+          >
+
+            <div className="box">
+
+                <input 
+                type="text" 
+                placeholder="City" 
+                value={this.state.city}
+                onChange = {e => this.updateState(e)}
+                className='input'/>
+
+                <button 
+                type="submit"
+                className="submit"> 
+
+                Add
+                </button>
+            </div>
+          </form>
+          )
+      }
+    };
+
+export default Nav
